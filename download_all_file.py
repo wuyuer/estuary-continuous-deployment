6af56e7d3e47f18f66dfd65b1127d2c05712ea8a
Elaine Wu <wuyanjun1027@163.com>
2016-03-04 11:16:03 +0800

    delete the hard code in download_all_file.py

--- download_all_file.py
+++ download_all_file.py
@@ -13,11 +13,8 @@ from urllib2 import URLError
 import logging
 import subprocess
 
-url = "http://192.168.1.108:8083"
-token = "3eda8013-da37-42ea-b9a0-7a66badd1b68"
-
 parse_re = re.compile('href="([^./"?][^"?]*)"')
-def get_urlname(git_describe, job_tree):
+def get_urlname(git_describe, job_tree, url, token):
     array = [url, job_tree, git_describe]
     urlname = '/'.join(array) 
     try:
@@ -68,20 +65,26 @@ def download(lists):
     
 if __name__ == "__main__":
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "d:j:")
+        opts, args = getopt.getopt(sys.argv[1:], "d:j:u:t")
     except getopt.GetoptError as err:
         print str(err)
         sys.exit(2)
     direc = ''
     job = ''
+    url = "http://192.168.1.108:8083"
+    token = "3eda8013-da37-42ea-b9a0-7a66badd1b68"
     for option, value in opts:
         if option == "-d":
             direc = value
         if option == "-j":
             job = value
+        if option == "-u":
+            url = value
+        if option == "-t":
+            token = value
     if not direc and not job:
         exit(-1)
-    urlname = get_urlname(git_describe=direc, job_tree=job)
+    urlname = get_urlname(git_describe=direc, job_tree=job, url=url, token=token)
     #print urlname
     walk(urlname)
     #print address_list
